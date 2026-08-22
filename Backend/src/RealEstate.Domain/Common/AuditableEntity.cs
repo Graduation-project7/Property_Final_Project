4292340acedfbf0013731d317aec928f1f0868c3
Ahmad7757 <ahmadbesherdabbagh@gmail.com>
2026-08-22 17:16:40 +0300

    feat: add entities for domain layer

---
+++ Backend/src/RealEstate.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstate.Domain.Common;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    protected AuditableEntity(int id) : base(id)
+    {
+
+    }
+    public DateTimeOffset CreatedAtUtc { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTimeOffset LastModifiedUtc { get; set; }
+
+    public string? LastModifiedBy { get; set; }
+
+}
---
+++ Backend/src/RealEstate.Domain/Entities/PropertyImg.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstate.Domain.Entities
+{
+    public class PropertyImg
+    {
+        public string? ImgUrl { get; private set; }
+    }
+}
